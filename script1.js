e4799e13b94d150bc5a243ed3f042d79b014849f
Chauncey Plummer <chaunceyp@tapcxm.com>
2024-12-03 10:32:33 -0500

    added FIREBASE sdk code

--- script1.js
+++ script1.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getMessaging, getToken } from 'firebase/messaging'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyAkX9vM0SfqKJmx2xbzYoEq8SjCjAbyDrk',
-  authDomain: 'aep-project-eb6b4.firebaseapp.com',
-  projectId: 'aep-project-eb6b4',
-  storageBucket: 'aep-project-eb6b4.firebasestorage.app',
-  messagingSenderId: '810654794951',
-  appId: '1:810654794951:web:fea774949c7e8edda12ae2',
-  measurementId: 'G-NYZ8FGYDSN',
+const messaging = getMessaging()
+// Add the public key generated from the console here.
+getToken(messaging, {
+  vapidKey:
+    'BJNAl-xOuYkL8VUSNxqVpVDHHwk63TCSk-n8rfdjmCuftqzVtg1LtpXJvXjiAQyozBUCb25Vv2DFP72phYRA2TU',
+})
+function requestPermission() {
+  console.log('Requesting permission...')
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.')
+    }
+  })
 }
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
