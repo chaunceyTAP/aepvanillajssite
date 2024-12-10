2758b1965ef8796a814a0c2814714412e9f1e8ac
chauncey plummer <chaunceyp@tapcxm.com>
2024-12-10 13:19:50 -0500

    added new surface

--- script.js
+++ script.js
@@ -25,9 +25,10 @@ alloy('sendEvent', {
     `this is returned from the code based experience${JSON.stringify(res)}`
   )
   if (res.decisions) {
-    const content = (res.decisions.items.data[
-      'content'
-    ].document.getElementById('#cp-code-based-html').innerHTML = content)
+    const con = res.decisions.items.data.content
+    const content = (con.document.getElementById(
+      '#cp-code-based-html'
+    ).innerHTML = content)
     console.log('update the dom with the code based experience')
   } else {
     console.error('No experience content received.')
